5bb835af2c3852dc66ac0b21c8092914bede31c1
Tom01 <nidzatomic01@gmail.com>
2023-09-24 20:12:03 +0200

    init

---
+++ src/main/java/com/example/weatherwebapp/WeatherWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weatherwebapp/WeatherWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
